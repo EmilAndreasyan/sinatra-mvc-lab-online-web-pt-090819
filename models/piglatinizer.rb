19395ff19532e255f066d0b00e5dead1896863cc
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-12-15 00:04:46 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,21 +1,27 @@
 class PigLatinizer
-  attr_reader :text
-  
-  def initialize(text)
-    @text = text.downcase
-  end
-  
-  def latinizer
-    word1 = text.split(" ")[0]
-    word2 = text.split(" ")[1]
-    
-    consonants2 = word2..split("").scan(/[bcdfghjklmnpqrstvwxyz]/)
-    
-    word1.map do |letter|
-      consonants1 = word1.split("").scan(/[bcdfghjklmnpqrstvwxyz]/)
-      if letter == consonants1
+
+  def piglatinize(word)
+    return word if %w[and an in].include?(word) #one syllable exceptions
+    letters = word.split("")
+    letters.keep_if {|letter| letter != "."}
+    if letters.size > 1
+      until vowel?(letters[0]) 
+        letters << letters.shift
+      end
+      letters  << "ay"
     end
-    
+    letters.join
   end
-  
-end
+
+  def to_pig_latin(text)
+    words = text.split(" ")
+    words.map! {|word| piglatinize(word)}
+    words.join(" ")
+  end
+
+  def vowel?(letter)
+    letter.downcase
+    letter == "o" || letter == "e" || letter == "a" || letter == "i" || letter == "u"
+  end
+
+end 
